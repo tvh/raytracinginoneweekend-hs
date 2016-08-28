c0bcad16f9abc478b5245e35ecadef9361b90821
Timo von Holtz <vonholtz@cpmed.com>
2016-08-28 08:48:42 +0200

    output to png

--- src/Main.hs
+++ src/Main.hs
@@ -3,17 +3,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import RaytracingBook.Hitable
-import RaytracingBook.Sphere
-import RaytracingBook.Ray
 import RaytracingBook.Camera
+import RaytracingBook.Hitable
+import RaytracingBook.Ray
+import RaytracingBook.Sphere
 
-import Linear
+import Codec.Picture
 import Control.Lens
-import qualified Data.Vector as V
-import Control.Monad
-import System.Random.MWC
+import Linear
+import System.Environment
 import System.IO.Unsafe
+import System.Random.MWC
+import qualified Data.Vector as V
 
 gen :: GenIO
 gen = unsafePerformIO createSystemRandom
@@ -25,10 +26,10 @@ drand48 = (+ (-2**(-33))) <$> uniform gen
 randomInUnitSphere :: IO (V3 Float)
 randomInUnitSphere = do
     rnd <- V3 <$> drand48 <*> drand48 <*> drand48
-    let vector = 2*rnd - 1
-    if dot vector vector >= 1
+    let res = 2*rnd - 1
+    if dot res res >= 1
       then randomInUnitSphere
-      else pure vector
+      else pure res
 
 color :: Ray Float -> HitableList Float -> IO (V3 Float)
 color ray world =
@@ -44,29 +45,22 @@ color ray world =
 
 main :: IO ()
 main = do
-    let nx = 200 :: Int
-        ny = 100 :: Int
+    [out] <- getArgs
+    let nx = 400 :: Int
+        ny = 200 :: Int
         ns = 100 :: Int
-    putStrLn $ "P3\n" ++ show nx ++ " " ++ show ny ++ "\n255"
-    let lower_left_corner :: V3 Float = V3 (-2) (-1) (-1)
-        horizontal :: V3 Float = V3 4 0 0
-        vertical :: V3 Float = V3 0 2 0
-        origin :: V3 Float = V3 0 0 0
         sphere1 = Sphere{ _center = V3 0 0 (-1), _radius = 0.5 }
         sphere2 = Sphere{ _center = V3 0 (-100.5) (-1), _radius = 100 }
         world = HitableList $ V.map (HitableItem . hit) [sphere1,sphere2]
         camera = defaultCamera
-    V.forM_ [ny-1,ny-2..0] $ \j ->
-        V.forM_ [0..nx-1] $ \i -> do
-            cols <- V.forM [1..ns] $ \_ ->
-                do uD <- drand48
-                   vD <- drand48
-                   let u = (fromIntegral i + uD) / fromIntegral nx
-                       v = (fromIntegral j + vD) / fromIntegral ny
-                       r = getRay camera u v
-                   color r world
-            let col = sqrt $ V.sum cols / fromIntegral ns
-            let ir :: Word = truncate $ (255.99 *) $ col ^. _x
-                ig :: Word = truncate $ (255.99 *) $ col ^. _y
-                ib :: Word = truncate $ (255.99 *) $ col ^. _y
-            putStrLn $ show ir ++ " " ++ show ig ++ " " ++ show ib
+    image <- withImage nx ny $ \i j -> do
+        cols <- V.forM [1..ns] $ \_ ->
+            do uD <- drand48
+               vD <- drand48
+               let u = (fromIntegral i + uD) / fromIntegral nx
+                   v = (fromIntegral (ny - j) + vD) / fromIntegral ny
+                   r = getRay camera u v
+               color r world
+        let V3 ir ig ib = V.sum cols / fromIntegral ns
+        pure $ PixelRGBF ir ig ib
+    savePngImage out (ImageRGBF image)
