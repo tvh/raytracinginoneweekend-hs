4c804a992027b81cc1103c06b465fa2432e7035b
Timo von Holtz <tvh@tvholtz.de>
2016-08-30 23:11:22 +0200

    Actually test mwc-random in benchmark

--- src/Benchmarks.hs
+++ src/Benchmarks.hs
@@ -4,10 +4,7 @@ module Main where
 import qualified System.Random.MWC as MWC
 import qualified System.Random.Mersenne as MR
 import qualified System.Random.Mersenne.Pure64 as MRP
-import qualified System.Random as SR
 import System.IO.Unsafe
-import Control.Arrow
-import Data.IORef
 import Data.Word
 import Data.Int
 import Data.Bits
@@ -22,20 +19,6 @@ genMWC = unsafePerformIO MWC.create
 {-# NOINLINE genMR #-}
 genMR :: MR.MTGen
 genMR = unsafePerformIO MR.getStdGen
-
-{-# NOINLINE genStd #-}
-genStd :: IORef SR.StdGen
-genStd = unsafePerformIO $ newIORef =<< SR.newStdGen
-
-getStdRandomNT :: (SR.StdGen -> (a,SR.StdGen)) -> IO a
-getStdRandomNT f = do
-    oldGen <- readIORef genStd
-    let (x,!newGen) = f oldGen
-    writeIORef genStd newGen
-    return x
-
-randomIONT :: SR.Random a => IO a
-randomIONT = getStdRandomNT SR.random
 
 xorShift32 :: Word32 -> Word32
 xorShift32 !x1 =
@@ -68,19 +51,19 @@ xorShift64 !x1 =
 main :: IO ()
 main =
     defaultMain
-    [ bench "System.Random.randomIO (threadsafe)" $ whnfIO (SR.randomIO :: IO Float)
-    , bench "System.Random.randomIO (non-threadsafe)" $ whnfIO (randomIONT :: IO Float)
-    , bench "System.Random.random" $ whnf (SR.random :: SR.StdGen -> (Float,SR.StdGen)) (SR.mkStdGen 123456)
-    , bench "MWC.uniform" $ whnfIO (SR.randomIO :: IO Float)
-    , bench "MR.randomIO (Word64)" $ whnfIO (MR.randomIO :: IO Word64)
-    , bench "MR.randomIO (Double)" $ whnfIO (MR.randomIO :: IO Double)
-    , bench "MR.random (Word64)" $ whnfIO (MR.random genMR :: IO Word64)
-    , bench "MR.random (Double)" $ whnfIO (MR.random genMR :: IO Double)
-    , bench "MRP.random (Word64)" $ whnf MRP.randomWord64 (MRP.pureMT 12345678)
-    , bench "MRP.random (Double)" $ whnf MRP.randomDouble (MRP.pureMT 12345678)
-    , bench "MRP.random (Float)" $ whnf (first (realToFrac :: Double -> Float) . MRP.randomDouble) (MRP.pureMT 12345678)
-    , bench "xorShift32" $ whnf xorShift32 314159265
-    , bench "xorShift32Float" $ whnf xorShift32Float 314159265
-    , bench "xorShift64" $ whnf xorShift64 88172645463325252
-    , bench "xorShift128+" $ whnf XOS.next (XOS.initialize 123456)
+    [ bench "MWC.uniform (Word32)" $ nfIO (MWC.uniform genMWC :: IO Word32)
+    , bench "MWC.uniform (Word64)" $ nfIO (MWC.uniform genMWC :: IO Word64)
+    , bench "MWC.uniform (Float)" $ nfIO (MWC.uniform genMWC :: IO Float)
+    , bench "MWC.uniform (Double)" $ nfIO (MWC.uniform genMWC :: IO Double)
+    , bench "MR.randomIO (Word64)" $ nfIO (MR.randomIO :: IO Word64)
+    , bench "MR.randomIO (Double)" $ nfIO (MR.randomIO :: IO Double)
+    , bench "MR.random (Word64)" $ nfIO (MR.random genMR :: IO Word64)
+    , bench "MR.random (Double)" $ nfIO (MR.random genMR :: IO Double)
+    , bench "MRP.random (Word64)" $ nf (fst . MRP.randomWord64) (MRP.pureMT 12345678)
+    , bench "MRP.random (Double)" $ nf (fst . MRP.randomDouble) (MRP.pureMT 12345678)
+    , bench "MRP.random (Float)" $ whnf ((realToFrac :: Double -> Float) . fst . MRP.randomDouble) (MRP.pureMT 12345678)
+    , bench "xorShift32" $ nf xorShift32 314159265
+    , bench "xorShift32Float" $ nf xorShift32Float 314159265
+    , bench "xorShift64" $ nf xorShift64 88172645463325252
+    , bench "xorShift128+" $ nf (fst. XOS.next) (XOS.initialize 123456)
     ]
