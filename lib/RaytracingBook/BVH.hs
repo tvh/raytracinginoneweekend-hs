a794d63223a6ccd94fd26b5fcc491bb9e77c0507
Timo von Holtz <tvh@tvholtz.de>
2016-09-06 08:46:20 +0200

    Don't use Rose trees for BoundingBox

--- lib/RaytracingBook/BVH.hs
+++ lib/RaytracingBook/BVH.hs
@@ -51,11 +51,15 @@ getBoundedHitableItem x =
     , bhi_centroid = centroid x
     }
 
-data BoundingBox f =
-    BoundingBox !(Point V3 f) !(Point V3 f) !(V.Vector (BoundedHitableItem f))
+data BoundingBox f
+    = BoundingBox !(Point V3 f) !(Point V3 f) !(BoundingBox f) !(BoundingBox f)
+    | BoundedNode !(BoundedHitableItem f)
+    | EmptyBoundingBox
 
 instance (Ord f, Fractional f) => Hitable f (BoundingBox f) where
-    hit (BoundingBox l h items) ray t_min t_max = do
+    hit EmptyBoundingBox _ _ _ = Nothing
+    hit (BoundedNode n) ray t_min t_max = hit n ray t_min t_max
+    hit (BoundingBox l h bb1 bb2) ray t_min t_max = do
         let t_x1 = (l^._x - ray^.ray_origin._x) / ray^.ray_direction._x
             t_x2 = (h^._x - ray^.ray_origin._x) / ray^.ray_direction._x
             t_min1 = max t_min (min t_x1 t_x2)
@@ -69,31 +73,30 @@ instance (Ord f, Fractional f) => Hitable f (BoundingBox f) where
             t_min3 = max t_min2 (min t_z1 t_z2)
             t_max3 = min t_max2 (max t_z1 t_z2)
         guard (t_max3 >= t_min3)
-        snd $ V.foldl' (merge t_min3) (t_max3, Nothing) items
+        let (t_max4, mClosest1) = merge t_min3 t_max3 Nothing bb1
+            (_     , mClosest2) = merge t_min3 t_max4 mClosest1 bb2
+        mClosest2
       where
-        merge :: f -> (f, Maybe (HitRecord f)) -> BoundedHitableItem f -> (f, Maybe (HitRecord f))
-        merge t_min (t_max, mClosest) item = do
+        merge :: f -> f -> Maybe (HitRecord f) -> BoundingBox f -> (f, Maybe (HitRecord f))
+        merge t_min t_max mClosest item = do
             case hit item ray t_min t_max `mplus` mClosest of
               Just closest -> (closest^.hit_t, Just closest)
               Nothing -> (t_max, Nothing)
 
 instance (Fractional f, Ord f) => BoundedHitable f (BoundingBox f) where
-    boundingBox (BoundingBox l h _) = (l,h)
+    boundingBox (BoundingBox l h _ _) = (l,h)
+    boundingBox (BoundedNode n) = boundingBox n
+    boundingBox EmptyBoundingBox = (0,0)
 
 data SplitDirection = X | Y | Z
 
-initializeBVH :: (Fractional f, Ord f) => V.Vector (BoundedHitableItem f) -> BoundingBox f
+initializeBVH :: forall f. (Fractional f, Ord f) => V.Vector (BoundedHitableItem f) -> BoundingBox f
 initializeBVH = go X
   where
+    go :: SplitDirection -> V.Vector (BoundedHitableItem f) -> BoundingBox f
     go direction v
-      | V.null v = BoundingBox 0 0 V.empty
-      | V.length v <= 5 =
-          let (l,h) =
-                  V.foldl1'
-                      (\(P l1, P h1) (P l2, P h2) ->
-                           (P (mzipWith min l1 l2), P (mzipWith max h1 h2)))
-                      (V.map boundingBox v)
-          in BoundingBox l h v
+      | V.null v = EmptyBoundingBox
+      | V.length v == 1 = BoundedNode $ V.unsafeHead v
       | otherwise =
           let v1_count = V.length v `div` 2
               splitted = runST $ do
@@ -101,11 +104,13 @@ initializeBVH = go X
                   VAI.selectBy (comparing (sortFun direction . centroid)) vm v1_count
                   V.unsafeFreeze vm
               (v1,v2) = V.splitAt v1_count splitted
-              bb1@(BoundingBox (P l1) (P h1) _) = go (next direction) v1
-              bb2@(BoundingBox (P l2) (P h2) _) = go (next direction) v2
+              bb1 = go (next direction) v1
+              bb2 = go (next direction) v2
+              (P l1, P h1) = boundingBox bb1
+              (P l2, P h2) = boundingBox bb2
               l = mzipWith min l1 l2
               h = mzipWith max h1 h2
-          in BoundingBox (P l) (P h) (V.fromList $ map getBoundedHitableItem [bb1, bb2])
+          in BoundingBox (P l) (P h) bb1 bb2
     sortFun :: SplitDirection -> Point V3 f -> f
     sortFun X = view _x
     sortFun Y = view _y
